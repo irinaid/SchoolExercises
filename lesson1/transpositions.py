dbf24e25e487dd09a7c9de25755d0f0a072cec0f
IrinaDmt <IrinaDmt@users.noreply.github.com>
2015-02-25 14:20:26 +0000

    nested lists

--- lesson1/transpositions.py
+++ lesson1/transpositions.py
@@ -39,18 +39,37 @@ photos_album = [tulips, pencils, bliss, bird]
 
 # Make a new directory
 mkdir("album")
-
+album45 = []
 # Iterate over the album and change all photos at once
 for i in range(0, len(photos_album)):
   image = photos_album[i].rotate(45)
   image.save("album/image" + str(i), "JPEG")
+  album.append(image)
 
+mkdir("rotated_pics")
+album180 = []
 # Iterate over the list to make other transformations!
 for i in range(0, len(photos_album)):
   image = photos_album[i].rotate(180)
-  image.save("album/rotated_image" + str(i), "JPEG")
-  
+  image.save("rotated_pics/image" + str(i), "JPEG")
+  album180.append(image)
+
+mkdir("flipped_pics")
+albumflipped = []
 for i in range(0, len(photos_album)):
   image = photos_album[i].transpose(Image.FLIP_LEFT_RIGHT)
-  image.save("album/flipped_image" + str(i), "JPEG")
+  image.save("flipped_pics/image" + str(i), "JPEG")
+  albumflipped.append(image)
+
+# Make a list of all the albums
+# This is a list of lists!
+albums = [album45, album180, albumflipped]
+
+# Iterate over every image in every album to get their sizes
+for i in range(0, len(albums)):
+  for j in range(0, len(albums[i])):
+    width, height= albums[i][j].size
+    print width, height
+    # Add any other changes you like!
+
   
